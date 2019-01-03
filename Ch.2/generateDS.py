0270407d0d84347681664d2a99ca6fd3b7fcc7e1
Deep <pandeydeep9g@gmail.com>
2019-01-03 17:30:50 +0545

    Generate dummy dataset. See what it looks like

---
+++ Ch.2/generateDS.py
@@ -0,0 +1,21 @@
+#simple script to generate a dataset
+import pylab
+import numpy
+
+# 100 data points between -1 and 1
+x= numpy.linspace(-1,1,100)
+
+signal = 2 + x - 2 * x**3
+noise = numpy.random.normal(0,0.2,100)
+
+y = signal + noise
+
+pylab.plot(signal,'b')
+pylab.plot(y,'g')
+pylab.plot(noise,'r')
+
+pylab.xlabel('x')
+pylab.ylabel('y')
+pylab.legend(["Without noise", "With Noise", "Noise"], loc = 2)
+
+pylab.show()
