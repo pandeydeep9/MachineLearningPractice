41d042a9a35d9fa0bc62f2831e85291f9dae2427
Deep <pandeydeep9g@gmail.com>
2019-01-05 13:30:42 +0545

    Theano function with vectors/matrix

--- Ch.4/theanoFunctions.py
+++ Ch.4/theanoFunctions.py
@@ -1,19 +1,29 @@
+#now with vectors
+import numpy as np
 import theano.tensor as T
 from theano import function
 
-#scalars defined before they can be used, every scalar given a unique name
-a = T.dscalar('a')
-b = T.dscalar('b')
-c = T.dscalar('c')
-d = T.dscalar('d')
-e = T.dscalar('e')
+#matxis defined now before they can be used, every matrix given a unique name
+a = T.dmatrix('a')
+b = T.dmatrix('b')
+c = T.dmatrix('c')
+d = T.dmatrix('d')
+#e = T.dmatrix('e')
 
+e = (a + b - c) * d
+f = function([a,b,c,d],e)#((a - b + c) * d) / e
 
-f = ((a - b + c) * d) / e
+a_data = np.array([[1,2],[4,3]]) #Just a simple matrix
+b_data = np.array([[5,6],[7,8]])
+c_data = np.array([[9,10],[11,12]])
+d_data = np.array([[13,14],[15,16]])
+#print(a_data[1][0])
 
-#function is a theano construct  which relates inputs(a,b,c,d,e) to outputs(f)
-g = function([a,b,c,d,e],f)
+normally = (a_data + b_data - c_data) * d_data
+fromTheano = f(a_data,b_data,c_data,d_data)
 
-print("Expected: ((1 - 2 + 3)* 4) / 5.0 =", ((1 - 2 + 3)* 4) / 5.0)
-print("Theano: ((1 - 2 + 3)* 4) / 5.0 =", g(1,2,3,4,5) )
+print("Expected: ", normally)
+print ("Theano:" , fromTheano)
+#print("Expected: ((1 - 2 + 3)* 4) / 5.0 =", ((1 - 2 + 3)* 4) / 5.0)
+#print("Theano: ((1 - 2 + 3)* 4) / 5.0 =", g(1,2,3,4,5) )
 
